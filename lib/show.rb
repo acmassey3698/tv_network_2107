382533fbbca6170b084f613c057a6de4d3909d82
Andrew Massey <acmassey@ncsu.edu>
2021-07-21 11:55:35 -0400

    Complete iteration two (show class)

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,50 @@
+require './lib/character'
 
+class Show
+
+  attr_reader :name,
+              :creator,
+              :characters
+
+
+  def initialize(name, creator, characters)
+    @name       = name
+    @creator    = creator
+    @characters = characters
+  end
+
+  def total_salary
+    salaries =
+    @characters.map do |character|
+      character.salary
+    end
+    salaries.sum do |salary|
+      salary
+    end
+  end
+
+  def max_salary
+    salaries =
+    @characters.map do |character|
+      character.salary
+    end
+    highest_salary =
+    salaries.max do |salary_1, salary_2|
+      salary_1 <=> salary_2
+    end
+  end
+
+  def highest_paid_actor
+    highest_paid_character =
+    @characters.find do |character|
+      character.salary == max_salary
+    end
+    highest_paid_character.actor
+  end
+
+  def actors
+    @characters.map do |character|
+      character.actor
+    end
+  end
+end
